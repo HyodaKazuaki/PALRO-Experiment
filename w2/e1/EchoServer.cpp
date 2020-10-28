9b749473a6f6363d7bfd44654bff12b5f90b86fa
HyodaKazuaki <kazuaki@ex-t.jp>
2020-10-28 10:35:20 +0900

    add week 2

---
+++ w2/e1/EchoServer.cpp
@@ -0,0 +1,109 @@
+//////////////////////////////////////////////////////////////////
+// EchoServer　サーバプログラム                   	   		  	//
+// コンパイル方法： gcc -o EchoServer EchoServer.cpp -lstdc++		//
+// 実行方法： ./EchoServer 	                              	  	//
+// 動作内容： ひたすらクライアントの接続を待つ						//
+// 終了方法：　Ctrl+c                        	         	 	//
+//////////////////////////////////////////////////////////////////
+#include <stdio.h>		// fprintf, perrorの定義
+#include <stdlib.h>		// exitの定義
+#include <string.h>		// memsetの定義
+#include <signal.h>		// signal, SIGINTの定義
+#include <netdb.h>		// sockaddr_in, accept, AF_INET, SOCK_STREAM, socket, INADDR_ANY, htons, bind, listenの定義
+#include <unistd.h>		// close, read, writeの定義
+#include <fcntl.h>		// O_WRONLY, openの定義
+
+#define BUFSIZE 1024						// バッファサイズ
+#define PORT 54321							// ポート番号
+
+int sock = -1;
+int fd = -1;
+
+//////////////////////////////////////////////////////////////////
+// Ctrl+cを押したときの処理
+//////////////////////////////////////////////////////////////////
+void sigint_handler( int sig )
+{
+	fprintf( stderr, "sigint_hander()\n" );
+
+	if( sock >= 0 )	(void)close( sock );
+	if( fd >= 0 )	close( fd );
+	exit( 1 );
+}
+
+///////////////////////////////////////////////////////////////////////////
+// クライアントからの接続があったときの処理
+///////////////////////////////////////////////////////////////////////////
+void accept_and_echo()
+{
+	struct sockaddr_in caddr;				// ソケットアドレス構造体
+	int ret, wsize;
+	socklen_t len;
+	char buf[BUFSIZE];
+
+	len = sizeof( caddr );
+	fd = accept( sock, (struct sockaddr *)&caddr, &len );	// サーバソケットのハンドル取得
+	if( fd < 0 ){
+		perror( "accept" );
+		close( fd );
+		exit( 1 );
+	}
+	printf( "Connected to Client.\n" );
+	
+	/* ここに受信処理を記述 */
+	while(1){
+		read(fd, buf, BUFSIZE);
+		if (buf[0] == '\n') break;
+		printf("client: %s", buf);
+		write(fd, buf, BUFSIZE);
+	}
+
+	close( fd );
+	printf("Closed connection.\n");
+	
+}
+
+///////////////////////////////////////////////////////////////////////////
+// メイン関数
+///////////////////////////////////////////////////////////////////////////
+int main( void )
+{
+	int i;
+	struct sockaddr_in saddr;
+
+	signal( SIGINT, sigint_handler );							// ctrl+C入力の処理関数
+
+	if(( sock = socket( AF_INET, SOCK_STREAM, 0 )) < 0 ){		// ソケットの作成
+		perror( "sock" );
+		exit( 1 );
+	}
+
+	memset( (void*)&saddr, 0, sizeof( saddr ) );				// ソケットをメモリ領域に確保
+	saddr.sin_family = AF_INET;
+	saddr.sin_addr.s_addr = INADDR_ANY;
+	saddr.sin_port = htons(PORT);
+
+	int tmp= 1;
+	if( setsockopt( sock, SOL_SOCKET, SO_REUSEADDR, &tmp, sizeof( tmp ) ) ){
+		printf( "setsockopt() failed!\n" );
+	}
+	
+	if( bind( sock, (struct sockaddr *)&saddr, sizeof( saddr ) ) < 0 ){
+		perror( "bind" );
+		close( sock );
+		exit( 1 );
+	}
+
+	if( listen( sock, 1 ) < 0 ){
+		perror( "listen" );
+		close( sock );
+		exit( 1 );
+	}
+
+	// while( 1 ){
+		accept_and_echo();
+	// }
+
+	return 0;
+}
+
---
+++ w2/e1/ImageClientLocal.cpp
@@ -0,0 +1,97 @@
+//////////////////////////////////////////////////////////
+// ImageClientLocal クライアントプログラム				//
+// コンパイル方法： makefileを使用						//
+// 実行方法： ./ImageClientLocal						//
+// 動作内容： ビットマップファイルをサーバに転送		//
+// 終了方法： ビットマップファイル転送後自動終了		//
+//////////////////////////////////////////////////////////
+
+#include <netdb.h>						// gethostbynameの定義
+#ifdef __MACH__
+#include <opencv/highgui.h>					// CV_LOAD_IMAGE_ANYDEPTH, CV_LOAD_IMAGE_ANY_COLOR, cvLoadImage, cvSaveImage, CV_WINDOW_AUTOSIZE, cvNamedWindow, cvShowImage, cvWaitKey, cvDestroyWindowの定義
+#endif
+#ifndef __MACH__
+#include <highgui.h>					// CV_LOAD_IMAGE_ANYDEPTH, CV_LOAD_IMAGE_ANY_COLOR, cvLoadImage, cvSaveImage, CV_WINDOW_AUTOSIZE, cvNamedWindow, cvShowImage, cvWaitKey, cvDestroyWindowの定義
+#endif
+#include <stdio.h>
+#include <fcntl.h>						// O_RDONLY　を使用するため
+#include <unistd.h>						// read, close　など
+#include <netdb.h>						// gethostbynameの定義
+
+#define BMP_FILE_NAME "savedImage.bmp"	// 送信するファイル名
+#define BUFF_SIZE 1024					// バッファサイズ
+#define PORT 54321						// ポート番号
+#define HOST_NAME "localhost"			// サーバ名
+	
+///////////////////////////////////////////////////////////////////////////
+// ネットワーク開始処理
+// 引数：なし
+// 返り値：ソケットのディスクリプター
+///////////////////////////////////////////////////////////////////////////
+int OpenNetwork( )
+{
+	int sock;									// ここから一般的なソケット処理
+	if(( sock = socket( AF_INET, SOCK_STREAM, 0 )) < 0 ){
+		perror( "sock" );
+		return -1;
+	}
+
+	struct hostent *hp = gethostbyname( HOST_NAME );
+	if( hp == NULL ){
+		perror( "socket" );
+		return -1;
+	}
+
+	struct sockaddr_in addr;
+	memset( (void*)&addr, 0, sizeof( addr) );
+	memcpy( &addr.sin_addr, hp->h_addr, hp->h_length );
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT);
+
+	if( connect( sock, (struct sockaddr *)&addr, sizeof( addr ) ) < 0 ){
+		perror( "connect" );
+		return -1;
+	}
+	return sock;
+}
+
+///////////////////////////////////////////////////////////////////////////
+// ネットワーク終了処理
+// 引数：ソケットディスクリプター
+// 返り値：正常終了0、異常終了-1
+///////////////////////////////////////////////////////////////////////////
+int CloseNetwork( int sock )
+{
+	close( sock );
+
+	return 0;
+}
+		
+///////////////////////////////////////////////////////////////////////////
+// メイン関数
+///////////////////////////////////////////////////////////////////////////
+int main( void )
+{
+	// ネットワーク接続
+	int sock = OpenNetwork();						// ソケットのハンドル取得
+
+	int bfd = open( BMP_FILE_NAME, O_RDONLY );		// ビットマップファイルをバイナリ形式でオープン
+	if( bfd < 0 ){
+		return 1;
+	}
+
+	char buf[BUFF_SIZE];							// データ転送用配列
+	long bmpSize = 0;
+
+	/* ここに送信処理を挿入 */
+
+	close( bfd );									// ビットマップファイルのクローズ
+				
+	strcpy( buf, "message end" );
+	write( sock, buf, strlen( buf ) + 1 );
+	CloseNetwork( sock );
+
+	printf( "%ld KB\n", bmpSize * BUFF_SIZE / 1024);
+		
+}
+
