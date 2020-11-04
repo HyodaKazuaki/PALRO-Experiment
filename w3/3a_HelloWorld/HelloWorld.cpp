d0d3b1e1a1820d96ccc3903ec212cba6b86f9a16
HyodaKazuaki <kazuaki@ex-t.jp>
2020-11-04 10:14:50 +0900

    first files

---
+++ w3/3a_HelloWorld/HelloWorld.cpp
@@ -0,0 +1,46 @@
+//////////////////////////////////////////////
+// HelloWorldプログラム						//
+// コンパイル方法： makefileを使用			//
+// 実行方法： プログラム転送サービス検証用		//
+// 動作内容： 「こんにちは」　と言う			//
+// 終了方法： 自動終了						//
+//////////////////////////////////////////////
+
+#include "TransientApplication.h"		// PALROの非常駐型アプリケーション関係
+#include "LogCtrl.h"					// PALROのログ関係
+
+class CHelloWorld : public PAPI::CTransientApplication
+{
+	
+public:
+///////////////////////////////////////////////////////////////////////////
+// 発話関数
+// 引数：PALROに話させたい文字列
+// 返り値：0: 正常にイベント待ちが完了、0以外: タイムアウトが発生した、イベント待ちが正常に完了しなかった
+///////////////////////////////////////////////////////////////////////////
+	long mySpeak( std::string str )
+	{
+		PAPI::CCommParams cParam;											// 発話パラメータ準備
+		cParam.Set( PAPI::PARAM_SENTENCE, str );
+		long lReqno = cComm.Send(PAPI::COMTYP_TELL, NULL, cParam);			// 発話開始
+		long lResult = evDispatcher.Sync(lReqno);							// 発話完了待ち
+		if (lResult != 0) {													// エラー判定
+			SAPIE_LOG_ERR( 0, "Error: %ld", lResult );
+		}
+		return lResult;
+	}
+
+///////////////////////////////////////////////////////////////////////////
+// メイン関数
+///////////////////////////////////////////////////////////////////////////
+	void OnProcessMain(Sapie::CControllerBase* pSender)
+	{
+		mySpeak( "こんにちは" );
+	}
+};
+
+void AppMain(Sapie::CApplicationInitializer& initializer)
+{
+	// アプリケーションのインスタンスを作成して、APIへ通知する
+	initializer.SetApplication(new CHelloWorld());
+}
